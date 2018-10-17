f4cbda98b77215e7e6f91ae295a899012a917d3a
FelipeToniolo <ftoniolo.aat@gmail.com>
2018-10-17 20:30:48 -0300

    Comecando adicionar o Firebase no cadastro

---
+++ app/src/main/java/com/example/felipetoniolo/organizze/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.felipetoniolo.organizze.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.felipetoniolo.organizze.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/felipetoniolo/organizze/activity/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.felipetoniolo.organizze.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.felipetoniolo.organizze.R;
+import com.example.felipetoniolo.organizze.activity.CadastroActivity;
+import com.example.felipetoniolo.organizze.activity.LoginActivity;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .canGoForward(false)
+                .build());
+    }
+
+    public void btn_entrar (View view){
+        startActivity(new Intent(this, LoginActivity.class));
+
+    }
+
+    public void btn_cadastrar (View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+
+    }
+
+
+
+}
