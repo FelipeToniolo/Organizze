4836d70de13e74ba3b819faa204a347c4dfb54f7
FelipeToniolo <ftoniolo.aat@gmail.com>
2018-10-18 20:45:37 -0300

    ëqualize master

--- app/src/main/java/com/example/felipetoniolo/organizze/activity/MainActivity.java
+++ app/src/main/java/com/example/felipetoniolo/organizze/activity/MainActivity.java
@@ -1,8 +1,14 @@
-package com.example.felipetoniolo.organizze;
+package com.example.felipetoniolo.organizze.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.felipetoniolo.organizze.R;
+import com.example.felipetoniolo.organizze.activity.CadastroActivity;
+import com.example.felipetoniolo.organizze.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -42,4 +48,17 @@ public class MainActivity extends IntroActivity {
                 .canGoForward(false)
                 .build());
     }
+
+    public void btn_entrar (View view){
+        startActivity(new Intent(this, LoginActivity.class));
+
+    }
+
+    public void btn_cadastrar (View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+
+    }
+
+
+
 }
